1b06aa79bb940e3851cbb1067f44a00855a87671
Peter Gill <peter.gill@townsuite.com>
2016-03-28 11:19:14 -0230

    Encrypt tool

---
+++ EncyptTool/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncyptTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = args[0];
+            bool useHash = true;
+            bool.TryParse(args[1], out useHash);
+            var key = args[2];
+
+            Console.Write(Encrypt(value, useHash, key));
+        }
+
+        private static string Encrypt(string toEncrypt, bool useHashing, string key)
+        {
+            var toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+            byte[] keyArray;
+
+            if (useHashing)
+            {
+                using (var hashmd5 = new MD5CryptoServiceProvider())
+                {
+                    keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+                    hashmd5.Clear();
+                }
+            }
+            else
+            {
+                keyArray = UTF8Encoding.UTF8.GetBytes(key);
+            }
+
+            using (var tdes = new TripleDESCryptoServiceProvider())
+            {
+                tdes.Key = keyArray;
+
+                tdes.Mode = CipherMode.ECB;
+                tdes.Padding = PaddingMode.PKCS7;
+
+                var cTransform = tdes.CreateEncryptor();
+
+                var resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+                tdes.Clear();
+
+                return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,21 @@
+A simple tool that can encrypt a string value that is passed in and prints to the console.
+
+# Arguments
+
+* "string value"
+* use hash
+* "thekey"
+
+# Examples
+
+```bash
+EncryptTool.exe "I will be encrypted" "true" "I am the encryption key"
+```
+
+The above encrypts using a hash.
+
+```bash
+EncryptTool.exe "I will be encrypted" "false" "I am the encryption key"
+```
+
+The above encrypts without using a hash.
